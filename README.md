9cf6c39301f3bf4443295c6cf22f0e2bc9c7ac2f
nightfallstorm <109926124+Nightfallstorm@users.noreply.github.com>
2022-10-09 22:22:29 -0500

    Initial Setup

---
+++ README.md
@@ -0,0 +1,34 @@
+# Skyrim Recursion FPS Fix
+
+TBD
+
+## Requirements
+* [CMake](https://cmake.org/)
+	* Add this to your `PATH`
+* [PowerShell](https://github.com/PowerShell/PowerShell/releases/latest)
+* [Vcpkg](https://github.com/microsoft/vcpkg)
+	* Add the environment variable `VCPKG_ROOT` with the value as the path to the folder containing vcpkg
+* [Visual Studio Community 2019](https://visualstudio.microsoft.com/)
+	* Desktop development with C++
+
+## User Requirements
+* [Address Library for SKSE](https://www.nexusmods.com/skyrimspecialedition/mods/32444)
+	* Needed for SSE
+* [VR Address Library for SKSEVR](https://www.nexusmods.com/skyrimspecialedition/mods/58101)
+	* Needed for VR
+
+## Register Visual Studio as a Generator
+* Open `x64 Native Tools Command Prompt`
+* Run `cmake`
+* Close the cmd window
+
+## Building
+```
+git clone https://github.com/Nightfallstorm/Skyrim-Recursion-FPS-Fix
+cd Skyrim-Recursion-FPS-Fix
+cmake --preset RecursionFix
+cmake --build build --config Release
+```
+
+## License
+[MIT](LICENSE)
---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+void InitializeLog()
+{
+	auto path = logger::log_directory();
+	if (!path) {
+		//stl::report_and_fail("Failed to find standard logging directory"sv); // Doesn't work in VR
+	}
+
+	*path /= Version::PROJECT;
+	*path += ".log"sv;
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::info);
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("[%H:%M:%S:%e] %v"s);
+
+	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
+}
+
+void InitializeHooking() {
+	logger::trace("Initializing StackFrameOverflow hook...");
+}
+
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+	SKSE::PluginVersionData v;
+	v.PluginVersion(Version::MAJOR);
+	v.PluginName("Skyrim Recursion FPS Fix");
+	v.AuthorName("nightfallstorm");
+	v.UsesAddressLibrary(true);
+	v.HasNoStructUse(true);
+
+	return v;
+}();
+
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
+{
+	a_info->infoVersion = SKSE::PluginInfo::kVersion;
+	a_info->name = Version::PROJECT.data();
+	a_info->version = Version::MAJOR;
+
+	return true;
+}
+
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+{
+	InitializeLog();
+	SKSE::Init(a_skse);
+	InitializeHooking();
+	logger::info("Loaded Plugin");
+	return true;
+}
+
+extern "C" DLLEXPORT const char* APIENTRY GetPluginVersion()
+{
+	return Version::NAME.data();
+}
